045142e601220622bcedd38e74528c58abd48d0d
elara-mendes <elaramendes@yahoo.com>
2025-01-23 16:11:01 -0300

    Using match case
    Today I learned about match case and strip method

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-message = "Enter a todo:"
-
 todos = []
 
 while True:
-    todo = input(message)
-    todos.append(todo.capitalize())
-    print(todos)
+    user_choice = input("Enter add, show or exit:").lower().strip()
+
+    match user_choice:
+        case "add":
+            todo = input("Enter a todo:")
+            todos.append(todo.capitalize())
+        case "show":
+            for todo in todos:
+                print(todo)
+        case "exit":
+            print("Bye!")
+            break
+        case _:
+            print("Write a known command!")
