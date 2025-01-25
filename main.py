2fa6214f2e8b2401c0d1f47ad25def8f2f165219
elara-mendes <elaramendes@yahoo.com>
2025-01-24 23:22:14 -0300

    Created edit case
    Today I learned about how to get an index item and edit it

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from dataclasses import replace
+
 todos = []
 
 while True:
-    user_choice = input("Enter add, show or exit:").lower().strip()
+    user_choice = input("Enter add, show, edit or exit:").lower().strip()
 
     match user_choice:
         case "add":
             todo = input("Enter a todo:")
-            todos.append(todo.capitalize())
+            todos.append(todo.title())
         case "show":
-            for todo in todos:
-                print(todo)
+            for index, todo in enumerate(todos):
+                print(index + 1, "-", todo)
+        case "edit":
+            user_edit_choice = int(input("Write the number of the todo:"))
+            user_new_word = input("Write your new todo:")
+            new_todo = todos[user_edit_choice - 1] = user_new_word.title()
         case "exit":
             print("Bye!")
             break
