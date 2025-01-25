a948d331f137f305fe3a031a5496f25ad613be61
elara-mendes <elaramendes@yahoo.com>
2025-01-25 16:16:17 -0300

    Created complete case
    Today I learned about f-strings and how to manipulate lists

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dataclasses import replace
 todos = []
 
 while True:
-    user_choice = input("Enter add, show, edit or exit:").lower().strip()
+    user_choice = input("Enter add, show, edit, complete or exit:").lower().strip()
 
     match user_choice:
         case "add":
@@ -11,11 +11,14 @@ while True:
             todos.append(todo.title())
         case "show":
             for index, todo in enumerate(todos):
-                print(index + 1, "-", todo)
+                print(f"{index + 1} - {todo}")
         case "edit":
             user_edit_choice = int(input("Write the number of the todo:"))
             user_new_word = input("Write your new todo:")
             new_todo = todos[user_edit_choice - 1] = user_new_word.title()
+        case "complete":
+            user_complete_choice = int(input("Which todo do you want to complete?"))
+            todos.pop(user_complete_choice - 1)
         case "exit":
             print("Bye!")
             break
