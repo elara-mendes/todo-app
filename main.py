85ee70f0c2428eec8cc995ddc02a178fa152057a
elara-mendes <elaramendes@yahoo.com>
2025-01-27 19:26:53 -0300

    Used an external file to store todos
    Today I learned how to store texts in a separate file to not lose my data

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from dataclasses import replace
-
-todos = []
+def write_file():
+    with open("todos.txt", "w") as text_file:
+        text_file.writelines(todos)
 
 while True:
     user_choice = input("Enter add, show, edit, complete or exit:").lower().strip()
 
+    with open("todos.txt", "r") as file:
+        todos = file.readlines()
+
     match user_choice:
         case "add":
-            todo = input("Enter a todo:")
+            todo = input("Enter a todo:") + "\n"
             todos.append(todo.title())
+            write_file()
         case "show":
             for index, todo in enumerate(todos):
                 print(f"{index + 1} - {todo}")
         case "edit":
             user_edit_choice = int(input("Write the number of the todo:"))
-            user_new_word = input("Write your new todo:")
+            user_new_word = input("Write your new todo:") + "\n"
             new_todo = todos[user_edit_choice - 1] = user_new_word.title()
+            write_file()
         case "complete":
             user_complete_choice = int(input("Which todo do you want to complete?"))
             todos.pop(user_complete_choice - 1)
+            write_file()
         case "exit":
             print("Bye!")
             break
