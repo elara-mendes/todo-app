0a198aabdfc9f67f9759e94c29a2e5a3f99f5061
elara-mendes <elaramendes@yahoo.com>
2025-02-04 14:19:57 -0300

    Improving code with try/except block

--- main.py
+++ main.py
@@ -9,37 +9,45 @@ while True:
     with open("todos.txt", "r") as file:
         todos = file.readlines()
 
-    if "add" in user_choice:
+    if user_choice.startswith("add"):
         todo = user_choice[4:]
         if todo != "":
             todo += "\n"
         # todo = input("Enter a todo:") + "\n"
             todos.append(todo.title())
             write_file()
-    elif "show" in user_choice: # Doesn't need more than this word.
+    elif user_choice.startswith("show"): # Doesn't need more than this word.
         new_todos = [todo.strip('\n') for todo in todos] # List Comprehension
 
         for index, todo in enumerate(new_todos):
             # todo = todo.strip("\n")
             print(f"{index + 1} - {todo}")
-    elif "edit" in user_choice:
-        todo = int(user_choice[4:])
-        # print(todo)
-        # user_edit_choice = int(input("Write the number of the todo:"))
-        user_new_word = input("Write your new todo:") + "\n"
-        todos[todo - 1] = user_new_word.title() # Python allows two attribution? Yes! but doesn't look good.
-        write_file()
-    elif "complete" in user_choice:
-        todo = int(user_choice[8:]) - 1
-        # print(todo)
-        if todo != "":
-            # user_complete_choice = int(input("Which todo do you want to complete?"))
-            # index = user_complete_choice - 1
-            remove_item = todos[todo].strip("\n")
-            todos.pop(todo)
-            print(f"The todo {remove_item} was removed.")
+    elif user_choice.startswith("edit"):
+        try:
+            todo = int(user_choice[4:])
+            # print(todo)
+            # user_edit_choice = int(input("Write the number of the todo:"))
+            user_new_word = input("Write your new todo:") + "\n"
+            todos[todo - 1] = user_new_word.title() # Python allows two attribution? Yes! but doesn't look good.
             write_file()
-    elif "exit" in user_choice:
+        except ValueError:
+            print("The command is incorrect.")
+            continue
+    elif user_choice.startswith("complete"):
+        try:
+            todo = int(user_choice[8:]) - 1
+            # print(todo)
+            if todo != "":
+                # user_complete_choice = int(input("Which todo do you want to complete?"))
+                # index = user_complete_choice - 1
+                remove_item = todos[todo].strip("\n")
+                todos.pop(todo)
+                print(f"The todo {remove_item} was removed.")
+                write_file()
+        except IndexError:
+            print("The value is incorrect.")
+            continue
+    elif user_choice.startswith("exit"):
         print("Bye!")
         break
     else:
