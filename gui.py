9b5873a25fa8d2fb4113e7aca87a1fe3546b57c3
elara-mendes <elaramendes@yahoo.com>
2025-02-19 16:37:28 -0300

    Created edit case

--- gui.py
+++ gui.py
@@ -7,10 +7,12 @@ todos = functions.read_file()
 text = sg.Text("Type in a todo")
 todo = sg.Input(tooltip="Write a todo here.", key="todo")
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values=todos, enable_events=True, size=(45, 10), key="list_item")
+edit_button = sg.Button("Edit")
 
 # Create the window
 window = sg.Window('My ToDo App',
-                   layout=[[text], [todo], [add_button]])
+                   layout=[[text], [todo, add_button], [list_box, edit_button]])
 
 while True:
     events, values = window.read()
@@ -24,7 +26,21 @@ while True:
             todos.append(todo_text)
             functions.write_file(todos)
 
+            window["todo"].update("")
+            window["list_item"].update(values=todos)
+
             print(f"The {print_text} was added with success!")
+        case "Edit":
+            edit_todo = values["list_item"][0]
+            new_todo = values["todo"]
+
+            # Getting Index and writing file
+            index_todo = todos.index(edit_todo)
+            todos[index_todo] = new_todo + "\n"
+            functions.write_file(todos)
+
+            window["todo"].update("")
+            window["list_item"].update(values=todos)
 
     if events == sg.WIN_CLOSED or events == 'Exit':
         break
