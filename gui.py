02b35d088d10a1a4ea8fd4dde01fb79b76a3035d
Elara <elaramendes@yahoo.com>
2025-02-20 23:56:32 -0300

    Created complete and exit case

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import functions
 import FreeSimpleGUI as sg
 
+
 # Reading file
 todos = functions.read_file()
 
@@ -9,10 +10,13 @@ todo = sg.Input(tooltip="Write a todo here.", key="todo")
 add_button = sg.Button('Add')
 list_box = sg.Listbox(values=todos, enable_events=True, size=(45, 10), key="list_item")
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 # Create the window
 window = sg.Window('My ToDo App',
-                   layout=[[text], [todo, add_button], [list_box, edit_button]])
+                   layout=[[text], [todo, add_button], [list_box, edit_button, complete_button],
+                           [exit_button]])
 
 while True:
     events, values = window.read()
@@ -41,7 +45,12 @@ while True:
 
             window["todo"].update("")
             window["list_item"].update(values=todos)
+        case "Complete":
+            complete_todo = values["list_item"][0]
+            todos.remove(complete_todo)
+            functions.write_file(todos)
 
+            window["list_item"].update(values=todos)
     if events == sg.WIN_CLOSED or events == 'Exit':
         break
 
